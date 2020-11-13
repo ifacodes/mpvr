f780809c0afe3ef77d1d2a64d5d35eee87bd1b52
[poww10s] <[theironminer@gmail.com]>
2020-11-12 21:22:22 -0500

    the next thing I tried

--- main.go
+++ main.go
@@ -8,12 +8,31 @@ import (
 
 // dial mpv pipe
 func dial() {
-	fmt.Println("Attempting to connect to pipe!")
-	_, err := winio.DialPipe("\\\\.\\pipe\\mpvsocket", nil)
+	conn, err := winio.DialPipe("\\\\.\\pipe\\mpvsocket", nil)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
+	conn2, err := winio.DialPipe("\\\\.\\pipe\\mpvsocket", nil)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	num, err := conn.Write([]byte("echo show-text ${playback-time}"))
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	fmt.Println("num: ", num)
+
+	var buf = make([]byte, 4096)
+	num, err = conn2.Read(buf)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	fmt.Println("num: {0}\nbuf: {1}", num, buf)
 }
 
 func main() {
